47f58ce11b1e4ff06ca13922bb9d8d5e9c8661c2
jo bars <jobars@protonmail.com>
2023-04-24 15:32:36 -0400

    it works! very cool

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::async_iter;
+use std::collections::HashMap;
 
 use firebase_rs::*;
 use serde::{Deserialize, Serialize};
@@ -40,4 +40,44 @@ async fn main() {
 
     delete_user(&firebase, &response.name).await;
     println!("User deleted.")
+}
+
+async fn set_user(firebase_client: &Firebase, user: &User) -> Response {
+    let firebase = firebase_client.at("users");
+    let _users = firebase.set::<User>(&user).await;
+    return string_to_response(&_users.unwrap().data);
+}
+
+async fn get_users(firebase_client: &Firebase) -> HashMap<String, User> {
+    let firebase = firebase_client.at("users");
+    let users = firebase.get::<HashMap<String, User>>().await;
+    println!("{:?}", users);
+    return users.unwrap();
+}
+
+async fn get_user(firebase_client: &Firebase, id: &String) -> User {
+    let firebase = firebase_client.at("users").at(&id);
+    let user = firebase.get::<User>().await;
+    return user.unwrap();
+}
+
+async fn update_user(firebase_client: &Firebase, id: &String, user: &User) -> User {
+    let firebase = firebase_client.at("users").at(&id);
+    let _user = firebase.update::<User>(&user).await;
+    return string_to_user(&_user.unwrap().data);
+}
+
+async fn delete_user(firebase_client: &Firebase, id: &String) {
+    let firebase = firebase_client.at("users").at(&id);
+    let _result = firebase.delete().await;
+}
+
+// convert a string to a response
+fn string_to_response(s: &str) -> Response {
+    serde_json::from_str(s).unwrap()
+}
+
+//convert a string to a user
+fn string_to_user(s: &str) -> User {
+    serde_json::from_str(s).unwrap()
 }
