7350a90286b7ad9c0fc4fa23c3ea1cd7e88265b8
jo bars <jobars@protonmail.com>
2023-04-24 01:24:26 -0400

    finished main rs

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use std::async_iter;
+
+use firebase_rs::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct User {
+    name: String,
+    age: u32,
+    email: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Response {
+    name: String,
+}
+
+#[tokio::main]
+async fn main() {
+    let user = User {
+        name: "ur mum".to_string(),
+        age: 29,
+        email: "ur.mum69@gmail.com".to_string(),
+    };
+
+    let firebase =
+        Firebase::new("https://rust-project-6b1af-default-rtdb.firebaseio.com/").unwrap();
+
+    let response = set_user(&firebase, &user).await;
+
+    let mut user = get_user(&firebase, &response.name).await;
+    println!("{:?}", user);
+
+    let users = get_users(&firebase).await;
+    println!("{:?}", users);
+
+    user.email = "updated.mum@gmail.com".to_string();
+    let updated_user = update_user(&firebase, &response.name, &user).await;
+    println!("{:?}", updated_user);
+
+    delete_user(&firebase, &response.name).await;
+    println!("User deleted.")
+}
